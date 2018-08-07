4fba0c01e9b2b75f1ae7cbecdb9cb7e5531d87f6
nathias <n@th1.as>
2018-08-07 18:04:24 +0200

    ascii box

---
+++ 2.js
@@ -0,0 +1,12 @@
+function logInBox(string) {
+  console.log('+-' + '-'.repeat(string.length) + '-+');
+  console.log('| ' + ' '.repeat(string.length) + ' |');
+  console.log('| ' + string + ' |');
+  console.log('| ' + ' '.repeat(string.length) + ' |');
+  console.log('+-' + '-'.repeat(string.length) + '-+');
+}
+
+
+logInBox('To boldly go where no one has gone before.');
+logInBox('');
+
