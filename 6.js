784b317364610cd1f8bc2f32e5e36e4445a294e6
nathias <n@th1.as>
2018-08-08 13:01:26 +0200

    6 madlibs

---
+++ 6.js
@@ -0,0 +1,18 @@
+// for web
+// var noun = prompt('Enter a noun: ');
+// var verb = prompt('Enter a verb:');
+// var adjective = prompt('Enter an adjective:');
+// var adverb = prompt('Enter an adverb:');
+
+var noun = 'dog';
+var verb = 'walk';
+var adjective = 'blue';
+var adverb = 'quickly';
+
+var sentence1 = 'Do you ' + verb + ' your ' + adjective + ' ' + noun + ' ' + adverb + "? That's hilarious!";
+var sentence2 = 'The ' + adjective + ' ' + noun + ' ' + verb + 's ' + adverb + ' over the lazy dog.';
+var sentence3 = 'The ' + noun + ' ' + adverb + ' ' + verb + 's up ' + adjective + " Joe's turtle.";
+
+console.log(sentence1);
+console.log(sentence2);
+console.log(sentence3);
